1b82645b200aaccddde621a525bf4218c8c82f79
Odinn breki <odinnbreki@outlook.com>
2025-03-26 17:58:35 +0000

    a

---
+++ lib/testrun.dart
@@ -0,0 +1,10 @@
+void main(){
+  print("banana");
+}
+int calcScore(List<int> hand){
+  int totalScore = 0;
+      for(int score in hand){
+        totalScore + score;
+      }
+      return totalScore;
+}
